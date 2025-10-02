f79d2ff283cb864c07a819eb05b08b347bfdbf52
Иван Алексеевич Коледов <ivan.koledov@chipdip.ru>
2025-10-02 16:49:00 +0300

    add textarea and calendar

--- src/pages/components/DiaryPanel/FormForEntries/FormForEntries.jsx
+++ src/pages/components/DiaryPanel/FormForEntries/FormForEntries.jsx
@@ -1,7 +1,18 @@
+import "@/pages/components/DiaryPanel/FormForEntries/FormForEntries.css"
+
 export default function FormForEntries() {
   return (
-    <div>
-      FormForEntries
+    <div className="form d-flex flex-column gap-2">
+      <textarea 
+        className="textarea-main" 
+        name="entry" 
+        id="entry" 
+        rows={15} 
+        cols={50} 
+        placeholder="Enter whatever you want :)">
+
+        </textarea>
+      <button className="textarea-btn btn btn-primary">Push</button>
     </div>
-  )
+  );
 }
--- src/pages/components/DiaryPanel/ListOfEntries/ListOfEntries.jsx
+++ src/pages/components/DiaryPanel/ListOfEntries/ListOfEntries.jsx
@@ -1,7 +1,13 @@
+import { useState } from 'react';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+
 export default function ListOfEntries() {
+  const [value, onChange] = useState(new Date());
+
   return (
     <div>
-      ListOfEntries
+      <Calendar onChange={onChange} value={value} />
     </div>
-  )
+  );
 }
