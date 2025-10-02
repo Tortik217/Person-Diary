26df0647d240def23f316e824758390c60647b96
Иван Алексеевич Коледов <ivan.koledov@chipdip.ru>
2025-10-02 17:21:07 +0300

    fix form

--- src/pages/components/DiaryPanel/FormForEntries/FormForEntries.jsx
+++ src/pages/components/DiaryPanel/FormForEntries/FormForEntries.jsx
@@ -1,18 +1,20 @@
-import "@/pages/components/DiaryPanel/FormForEntries/FormForEntries.css"
+import "@/pages/components/DiaryPanel/FormForEntries/FormForEntries.css";
 
 export default function FormForEntries() {
   return (
-    <div className="form d-flex flex-column gap-2">
-      <textarea 
-        className="textarea-main" 
-        name="entry" 
-        id="entry" 
-        rows={15} 
-        cols={50} 
-        placeholder="Enter whatever you want :)">
-
-        </textarea>
-      <button className="textarea-btn btn btn-primary">Push</button>
-    </div>
+    <form action="#" className="form d-flex flex-column gap-2">
+      <textarea
+        className="textarea-main"
+        name="entry"
+        id="entry"
+        rows={15}
+        cols={50}
+        placeholder="Enter whatever you want :)"
+        required
+      ></textarea>
+      <button type="submit" className="textarea-btn btn btn-primary">
+        Push
+      </button>
+    </form>
   );
 }
